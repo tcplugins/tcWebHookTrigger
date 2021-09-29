bcd5826ab3dc2b4eb0b4a7704a711f632fc65784
Net Wolf UK <netwolfuk@gmail.com>
2021-09-29 21:36:49 +1300

    Clean up logging

--- tcWebHookTrigger-server/src/main/java/teamcity/plugin/build/trigger/webhook/service/BuildTriggerResolverService.java
+++ tcWebHookTrigger-server/src/main/java/teamcity/plugin/build/trigger/webhook/service/BuildTriggerResolverService.java
@@ -25,15 +25,12 @@ public class BuildTriggerResolverService {
 	
 	public TriggersHolder findTriggersForBuildType(String buildTypeExternalId) throws BuildTypeNotFoundException, PermissionedDeniedException {
 		try {
-			Loggers.ACTIVITIES.debug(String.format("%s: Starting to look for SBuildType. buildType='%s'", LOGGING_PREFIX, buildTypeExternalId));
 			SBuildType sBuildType = myProjectManager.findBuildTypeByExternalId(buildTypeExternalId);
-			Loggers.ACTIVITIES.debug(String.format("%s: Completed looking for SBuildType. buildType='%s', found=%s", LOGGING_PREFIX, buildTypeExternalId, sBuildType != null));
+			Loggers.ACTIVITIES.debug(String.format("%s: Found matching SBuildType. buildType='%s'", LOGGING_PREFIX, buildTypeExternalId));
 			if (sBuildType == null) {
 				throw new BuildTypeNotFoundException(buildTypeExternalId);
 			}
-			Loggers.ACTIVITIES.debug(String.format("%s: Starting to look for SBuildType's triggers. buildType='%s'", LOGGING_PREFIX, buildTypeExternalId));
 			Collection<BuildTriggerDescriptor> buildTriggers = sBuildType.getBuildTriggersCollection();
-			Loggers.ACTIVITIES.debug(String.format("%s: Completed looking for SBuildType's triggers. buildType='%s', triggerCount=%s", LOGGING_PREFIX, buildTypeExternalId, buildTriggers.size()));
 			return new TriggersHolder(sBuildType,buildTriggers.stream()
 					.filter(trigger -> WebHookBuildTriggerService.WEBHOOK_BUILD_TRIGGER_NAME.equals(trigger.getBuildTriggerService().getName()))
 					.collect(Collectors.toList()));
